ade5948f77b294da304995961e1551d38b1fde0a
StaubDevLab <guillaume@staub.pro>
2025-03-26 22:21:48 +0100

    🔨 Refactor: adjust code warnings

--- app/api/creation/[id]/route.ts
+++ app/api/creation/[id]/route.ts
@@ -3,16 +3,17 @@ import { prisma } from "@/prisma";
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, context: { params: Promise<{ id: string }> }) {
     const session = await auth();
     if (!session || !session.user) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
     try {
+        const { id } = await context.params;
         await prisma.creation.delete({
             where: {
-                id: params.id,
+                id: id,
 
             },
         });
